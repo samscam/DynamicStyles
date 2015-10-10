fed19c10413f7f0834d4ab08c47928a7938f71df
Sam Easterby-Smith <sam@spotlightkid.co.uk>
2015-10-10 19:38:58 +0100

    Fixed example app - array of keys was using old syntax

--- DynamicStylesExample/DynamicStylesExample/ViewController.swift
+++ DynamicStylesExample/DynamicStylesExample/ViewController.swift
@@ -12,13 +12,20 @@ import DynamicStyles
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
-    var stylesheet: Stylesheet?
+    var stylesheet: Stylesheet!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        if let sheet = Stylesheet.defaultStylesheet {
+            stylesheet = sheet
+        } else {
+            return nil
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        stylesheet=Stylesheet.defaultStylesheet
             
         tableView.estimatedRowHeight=54
         tableView.rowHeight=UITableViewAutomaticDimension
@@ -39,8 +46,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("SampleCell") as! SampleCell
-        let key = self.stylesheet?.styles.keys.array[indexPath.row] as String?
-        let style = self.stylesheet?.style(key!)
+        let key = Array(self.stylesheet.styles.keys)[indexPath.row] as String?
+        let style = self.stylesheet.style(key!)
         cell.style=style
         return cell
     }
@@ -48,7 +55,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // MARK: UITableViewDelegate
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.stylesheet!.styles.count
+        return self.stylesheet.styles.count
     }
 
 
